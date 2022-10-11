13cc5db30101a343c6fd5f8f2f9eefabc9a44233
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2022-10-11 12:26:07 +0000

    ARM: imx6: spin the cpu until hardware takes it down

--- main.go
+++ main.go
@@ -61,4 +61,4 @@ func getStagedDiff() (string, error) {
 	}
 	result := strings.TrimSpace(string(output))
 	if result == "" {
-		return "", fmt.Errorf("No sta
+		return "", fmt.Errorf("No staged changes."
