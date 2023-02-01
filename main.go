9867504aa26de5a758e553f8b0e1a88238d7bae8
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2023-02-01 14:41:06 +0000

    more styling

--- main.go
+++ main.go
@@ -80,3 +80,4 @@ func fetchPrompt() (string, error) {
 		fmt.Println("No prompt file found. Creating one at ~/.config/acommit/prompt.txt")
 		const prompt = "You are to act as the author of a commit message in git. Your mission is to create clean and comprehensive commit messages in the gitmoji convention with emoji and explain why a change was done. I'll send you an output of 'git diff --staged' command, and you convert it into a commit message. Add a short description of WHY the changes are done after the commit message. Don't start it with 'This commit', just describe the changes. Use the present tense. Commit title must not be longer than 74 characters."
 		// mkdir -p
+		err = os.MkdirA
