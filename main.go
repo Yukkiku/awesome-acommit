1c8b9fcd6e82d22f230af0a0a1ea6dfa3db78227
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2023-04-11 22:23:02 +0000

    content updates

--- main.go
+++ main.go
@@ -104,4 +104,4 @@ func fetchPrompt() (string, error) {
 
 func generateText(diff string) (string, error) {
 	if apiKey == "" {
-		return "", fmt.Errorf("OPENAI_API_KEY en
+		return "", fmt.Errorf("OPENAI_API_KEY environment
