abd37145c00cc282a4a63badb47088dc1c537922
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2023-06-03 08:46:15 +0000

    reordering

--- main.go
+++ main.go
@@ -138,4 +138,4 @@ func generateText(diff string) (string, error) {
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiKey))
