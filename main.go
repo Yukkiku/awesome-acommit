bfe4912758a714df27ceabb5f5faaa374512de61
Yukkiku <106497277+Yukkiku@users.noreply.github.com>
2023-06-07 15:04:52 +0000

    WIP : Core

--- main.go
+++ main.go
@@ -141,4 +141,5 @@ func generateText(diff string) (string, error) {
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiKey))
 
 	resp, err := http.DefaultClient.Do(req)
-	if err != nil
+	if err != nil {
+		return 
